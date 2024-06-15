ca41bbf8ec901d5a3338fc33636569db9fae3067
lekonai <lekonai@users.noreply.github.com>
2024-06-15 23:06:13 +0100

    making SOMETHING at least.

--- jsFun/src/routes/+page.svelte
+++ jsFun/src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <!-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
+            <title>Home Page</title>
+            <!-- <link rel="stylesheet" href="style.css"> -->
+            <!-- with regular html, you'd usually keep this here, but not in this scenario -->
+        </head>
+        
+        <header>
+            <h1>Header, I'm the centre of it all.</h1>    
+        </header>
+        <div style="margin: 35;">
+            <h1>Placeholder Title</h1>
+            <p>I got no idea how to use this shit personally, I'm just figuring stuff as I go along.</p>
+            <button class="butt" type="button" >Go to next page.</button>
+
+
+
+
+        </div>
+        
+        <div id = "bottom">
+            <div id = "leftFoot">
+                <ul>
+                    <li><h3 style="font-size = 15;font-weight:normal">Made By O. Chowdhury</h3></li>
+                    <li><p><a href="https://kit.svelte.dev">kit.svelte.dev</a></p></li>
+
+                </ul>
+                
+            </div>
+            <div id = "rightFoot">
+                <img id = "cat" src="%sveltekit.assets%/cat.gif" alt="cat">
+            </div>
+        </div>
+    <body>
+</html>
+<style>
+    @import 'style.css';
+</style>
