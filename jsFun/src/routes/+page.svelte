c773f060b4b0b5e8736e32bebf8728df847e288d
lekonai <lekonai@users.noreply.github.com>
2024-06-17 00:32:02 +0100

    Commit Before I Mess Up Bad.

--- jsFun/src/routes/+page.svelte
+++ jsFun/src/routes/+page.svelte
@@ -9,31 +9,31 @@
             <!-- <link rel="stylesheet" href="style.css"> -->
             <!-- with regular html, you'd usually keep this here, but not in this scenario -->
         </head>
-        
-        <header>
-            <h1>Header, I'm the centre of it all.</h1>    
-        </header>
-        <div style="margin: 35;">
-            <h1>Placeholder Title</h1>
-            <p>I got no idea how to use this shit personally, I'm just figuring stuff as I go along.</p>
-            <button class="butt" type="button" >Go to next page.</button>
+        <div>
+            <h1 class = title>Welcome to the website!</h1>
+        </div>
+        <div class = "UnivHeader">
+            <h2 style="color: black;">Navigate: </h2>    
+        </div>
 
+        <div style="margin: 35px;">
+            <h1>Day 2: 16/06</h1>
+            <p>Still figuring out how to format correctly. Ensuring I got a nice layout before I do anything :)</p>
+            <button class="butt" type="button" >Go to next page.</button>
+            <p style="color: aqua;">yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/>yeah <br/></p>
+            
 
 
 
         </div>
         
-        <div id = "bottom">
-            <div id = "leftFoot">
-                <ul>
-                    <li><h3 style="font-size = 15;font-weight:normal">Made By O. Chowdhury</h3></li>
-                    <li><p><a href="https://kit.svelte.dev">kit.svelte.dev</a></p></li>
-
-                </ul>
-                
+        <div class = "bottom">
+            <div id = "leftFoot" style="margin-left: 25px; margin-top: 1px;">
+                <h3 style="font-size:20px;font-weight:normal">Made By O. Chowdhury</h3>
+                <p style="font-size: 11px;"><a href="https://kit.svelte.dev">svelte</a></p>
             </div>
             <div id = "rightFoot">
-                <img id = "cat" src="%sveltekit.assets%/cat.gif" alt="cat">
+                <img id = "cat" alt="cat">
             </div>
         </div>
     <body>
@@ -41,3 +41,15 @@
 <style>
     @import 'style.css';
 </style>
+<!-- <script>
+    window.onscroll = function() {headermove()};
+    var hddrr = document.getElementsByClassName("UnivHeader")
+    var sticky = hddrr.offsetTop;
+
+    function myFunction() {
+    if (window.pageYOffset > sticky) {
+        hddrr.classList.add("sticky");
+    } else {
+        hddrr.classList.remove("sticky");
+    }
+</script> -->
--- jsFun/vite.config.js
+++ jsFun/vite.config.js
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server:{
+		fs:{
+			allow: ['C:/Users/Omari/Documents/funny coding/js/realJsProject/jsFun/jsFun/static/HelveticaLight.ttf'],
+		},
+	},
 });
